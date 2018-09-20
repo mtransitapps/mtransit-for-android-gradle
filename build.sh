cd8084194b1d0a1d54c4cdab684023c3482a2219
Mathieu Méa <mathieu.mea@gmail.com>
2018-09-20 10:55:26 -0400

    Cleanup SH script

--- build.sh
+++ build.sh
@@ -7,22 +7,22 @@ BEFORE_DATE=$(date +%D-%X);
 BEFORE_DATE_SEC=$(date +%s);
 
 CURRENT_PATH=$(pwd);
-CURRENT_DIRECTORY=$(basename $CURRENT_PATH);
+CURRENT_DIRECTORY=$(basename ${CURRENT_PATH});
 CURRENT_DIRECTORY_LENGTH=${#CURRENT_DIRECTORY};
 GRADLE="-gradle";
 GRADLE_LENGTH=${#GRADLE};
 
 POSITION=0;
-LENGTH=$(expr $CURRENT_DIRECTORY_LENGTH - $GRADLE_LENGTH);
+LENGTH=$(expr ${CURRENT_DIRECTORY_LENGTH} - ${GRADLE_LENGTH});
 
-AGENCY_ID=${CURRENT_DIRECTORY:$POSITION:$LENGTH};
+AGENCY_ID=${CURRENT_DIRECTORY:${POSITION}:${LENGTH}};
 
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD);
 if [ "$GIT_BRANCH" = "HEAD" ]; then
 	GIT_BRANCH="";
 fi
 if [[ -z "${GIT_BRANCH}" ]]; then
-	GIT_BRANCH=$TRAVIS_BRANCH;
+	GIT_BRANCH=${TRAVIS_BRANCH};
 	if [ "$GIT_BRANCH" = "HEAD" ]; then
 		GIT_BRANCH="";
 	fi
@@ -40,10 +40,10 @@ IS_CI=false;
 if [[ ! -z "${CI}" ]]; then
 	IS_CI=true;
 fi
-echo "/build.sh: IS_CI '${IS_CI}'";
+echo "/build.sh > IS_CI:'${IS_CI}'";
 
 GRADLE_ARGS="";
-if [ $IS_CI = true ]; then
+if [ ${IS_CI} = true ]; then
 	GRADLE_ARGS=" --console=plain";
 fi
 
@@ -57,26 +57,26 @@ for d in ${PWD}/* ; do
 		echo "--------------------------------------------------------------------------------";
 		continue;
 	fi
-	if contains $DIRECTORY ${EXCLUDE[@]}; then
+	if contains ${DIRECTORY} ${EXCLUDE[@]}; then
 		echo "> Skip GIT cleaning in excluded directory '$DIRECTORY'.";
 		echo "--------------------------------------------------------------------------------";
 		continue;
 	fi
 	if [ -d "$d" ]; then
-		cd $d;
+		cd ${d};
 		echo "> GIT cleaning in '$DIRECTORY'...";
 		GIT_REV_PARSE_HEAD=$(git rev-parse HEAD);
-		GIT_REV_PARSE_REMOTE_BRANCH=$(git rev-parse origin/$GIT_BRANCH);
+		GIT_REV_PARSE_REMOTE_BRANCH=$(git rev-parse origin/${GIT_BRANCH});
 		if [ "$GIT_REV_PARSE_HEAD" != "$GIT_REV_PARSE_REMOTE_BRANCH" ]; then
 			echo "> GIT repo outdated in '$DIRECTORY' (local:$GIT_REV_PARSE_HEAD|origin/$GIT_BRANCH:$GIT_REV_PARSE_REMOTE_BRANCH).";
 			exit -1;
 		else
 			echo "> GIT repo up-to-date in '$DIRECTORY' (local:$GIT_REV_PARSE_HEAD|origin/$GIT_BRANCH:$GIT_REV_PARSE_REMOTE_BRANCH).";
 		fi
-		git checkout $GIT_BRANCH;
-		checkResult $? $CONFIRM;
+		git checkout ${GIT_BRANCH};
+		checkResult $? ${CONFIRM};
 		git pull;
-		checkResult $? $CONFIRM;
+		checkResult $? ${CONFIRM};
 		echo "> GIT cleaning in '$DIRECTORY'... DONE";
 		cd ..;
 		echo "--------------------------------------------------------------------------------";
@@ -87,10 +87,10 @@ echo "> BUILDING ANDROID APP FOR '$AGENCY_ID'...";
 cd app-android;
 
 ./bump_version.sh
-checkResult $? $CONFIRM;
+checkResult $? ${CONFIRM};
 
 ./build.sh
-checkResult $? $CONFIRM;
+checkResult $? ${CONFIRM};
 
 cd ..;
 echo "> BUILDING ANDROID APP FOR '$AGENCY_ID'... DONE";
