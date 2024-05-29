2c9efdfc571f940f2bc4567e2882cd6916fb95cc
Mathieu Méa <mathieu.mea@gmail.com>
2024-05-28 21:15:21 -0400

    Sync code

--- checkout_submodules.sh
+++ checkout_submodules.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # ORIGINAL FILE: https://github.com/mtransitapps/commons/tree/master/shared-overwrite
 #NO DEPENDENCY <= EXECUTED BEFORE GIT SUBMODULE
+function setGitProjectName() { # copy from commons.sh
+	GIT_URL=$(git config --get remote.origin.url);
+	echo "GIT_URL: '$GIT_URL'.";
+	GIT_PROJECT_NAME=$(basename -- ${GIT_URL});
+	GIT_PROJECT_NAME="${GIT_PROJECT_NAME%.*}" # remove ".git" extension
+	echo "GIT_PROJECT_NAME: '$GIT_PROJECT_NAME'.";
+	if [[ -z "${GIT_PROJECT_NAME}" ]]; then
+		echo "GIT_PROJECT_NAME not found!";
+		exit 1;
+	fi
+	PROJECT_NAME="${GIT_PROJECT_NAME%-gradle}";
+	echo "PROJECT_NAME: '$PROJECT_NAME'.";
+}
 
 echo "> GitHub Actions: $GITHUB_ACTIONS.";
 
@@ -18,15 +31,21 @@ else
 	echo "> Not a shallow GIT repo.";
 fi
 
+setGitProjectName;
+
 declare -a SUBMODULES=(
 	"commons"
 	"commons-java"
 	"commons-android"
-	"app-android"
 );
+if [[ $GIT_PROJECT_NAME == *"-gradle"* ]]; then # OLD REPO
+	SUBMODULES+=('app-android'); # OLD REPO
+fi
 if [[ -d "parser" ]]; then
     SUBMODULES+=('parser');
-    SUBMODULES+=('agency-parser');
+	if [[ $GIT_PROJECT_NAME == *"-gradle"* ]]; then # OLD REPO
+		SUBMODULES+=('agency-parser'); # OLD REPO
+	fi
 fi
 echo "Submodules:";
 printf '* "%s"\n' "${SUBMODULES[@]}";
